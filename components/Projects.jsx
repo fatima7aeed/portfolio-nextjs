61a14ef8602a2de2fc3573a81b9ce7e9b86a13cd
Fatima-saeed <fatima7aeed@gmail.com>
2023-08-02 21:04:48 +0500

    Modify Projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,10 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import propertyImg from '../public/assets/projects/property.jpg';
-import cryptoImg from '../public/assets/projects/crypto.jpg'
-import netflixImg from '../public/assets/projects/netflix.jpg'
-import twitchImg from '../public/assets/projects/twitch.jpg'
+import propertyImg from '../public/assets/projects/portfolio.png';
 import ProjectItem from './ProjectItem';
 
 const Projects = () => {
@@ -16,31 +13,31 @@ const Projects = () => {
         </p>
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
-          <ProjectItem
-            title='Property Finder'
-            backgroundImg={propertyImg}
-            projectUrl='/property'
-            tech='React JS'
-          />
-          <ProjectItem
-            title='Crypto App'
-            backgroundImg={cryptoImg}
-            projectUrl='/crypto'
-            tech='React JS'
+        <ProjectItem
+  title='My Portfolio'
+  backgroundImg={propertyImg}
+  projectUrl='https://fatimasaeed.vercel.app/' // Add your portfolio URL here
+  tech='React JS'
+          // />
+          // <ProjectItem
+          //   title='Crypto App'
+          //   backgroundImg={cryptoImg}
+          //   projectUrl='/crypto'
+          //   tech='React JS'
 
-          />
-          <ProjectItem
-            title='Netflix App'
-            backgroundImg={netflixImg}
-            projectUrl='/netflix'
-            tech='React JS'
+          // />
+          // <ProjectItem
+          //   title='Netflix App'
+          //   backgroundImg={netflixImg}
+          //   projectUrl='/netflix'
+          //   tech='React JS'
 
-          />
-          <ProjectItem
-            title='Twitch UI'
-            backgroundImg={twitchImg}
-            projectUrl='/twitch'
-            tech='Next JS'
+          // />
+          // <ProjectItem
+          //   title='Twitch UI'
+          //   backgroundImg={twitchImg}
+          //   projectUrl='/twitch'
+          //   tech='Next JS'
 
           />
         </div>
